6d27ec0f646ede82a477cc895b1fb5fc9ed10afc
ali hassan <146840063+notalihassan@users.noreply.github.com>
2024-09-04 16:22:43 +0000

    add

--- .ccls-cache/@home@runner@Cppfirst/conditionals@1question.cpp
+++ .ccls-cache/@home@runner@Cppfirst/conditionals@1question.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main(){
-  int n;
+// int main(){
+//   int n;
 
-  cout << "Enter the number : ";
-  cin >> n;
+//   cout << "Enter the number : ";
+//   cin >> n;
 
-  if( n % 2 == 0) {
-    cout << "Even" << endl;
-  }else{
-    cout << "Odd" << endl;
-  }
-  
-}
+//   if( n % 2 == 0) {
+//     cout << "Even" << endl;
+//   }else{
+//     cout << "Odd" << endl;
+//   }
+
+
+//   // 1. a = b means a is assigned the value of b
+//   // 2. a == b means a is equal to b
+//   // 3. a != b means a is not equal to b
+// }
