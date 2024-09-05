f4d62975219925be3a124265687b6c682c7f922f
ali hassan <146840063+notalihassan@users.noreply.github.com>
2024-09-05 16:36:52 +0000

    add

---
+++ .ccls-cache/@home@runner@Cppfirst/conditionals@threeDigitOrNot.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter the value : ";
+    cin >> n;
+
+    if (n > 99 && n < 1000)
+    {
+        cout << "3 digit number";
+    }
+    else
+    {
+        cout << "Not a 3 digit number";
+    }
+}
